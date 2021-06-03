babc9505c6c8fdb8ddeca8841178682e371036bc
esalling23 <esalling23@gmail.com>
2021-06-03 12:02:12 -0400

    Adds current headshot photo to About page

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -5,15 +5,20 @@ const About = () => (
 	<section id='about'>
 		<Row className='justify-content-center'>
 			<Col className='pb-3' xs={8} md={6}>
-				<Image src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' fluid/>
+				<Image
+					src='https://eron-portfolio.s3.amazonaws.com/Eron+Headshot+Big'
+					fluid
+				/>
 			</Col>
 			<Col xs={8} md={6}>
 				<p>
-					Hey there, I'm Eron Salling (he/him). I'm a game developer who specializes in
-					educational experiences and civic engagement. 
+					Hey there, I'm Eron Salling (he/him). I'm a game developer who
+					specializes in educational experiences and civic engagement.
 				</p>
 				<p>
-					When building games I tend to lean on Unity and C#, but I also enjoy building web applications using Django and ReactJS as well as the MERN stack. 
+					When building games I tend to lean on Unity and C#, but I also enjoy
+					building web applications using Django and ReactJS as well as the MERN
+					stack.
 				</p>
 			</Col>
 		</Row>
