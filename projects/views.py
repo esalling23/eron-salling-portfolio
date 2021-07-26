fbf0268e65db44b26eedb8b77dfb906a8d64919d
esalling23 <esalling23@gmail.com>
2021-07-26 12:49:37 -0400

    Project Index View now sorts by project start date, descending

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,6 @@ from .serializers import ProjectSerializer
 # Create your views here.
 def projects_index(request):
     """Index request to list all projects"""
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by('-date_started')
     serializer = ProjectSerializer(projects, many=True)
     return JsonResponse({ 'projects': serializer.data })
