90c32b6d0c94f50ad1aab5cbea060d5b8cc58d77
Eron Salling <esalling23@gmail.com>
2023-05-10 11:48:47 -0400

    Updates Project component organization
    Shows project categories
    Reorganizes details w/ see more component

--- frontend/src/components/shared/Project.js
+++ frontend/src/components/shared/Project.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Image, Col, Row, Badge } from 'react-bootstrap'
-import format from 'date-fns/format'
+import styled from 'styled-components'
 
-// Formats date into "Month NumYear"
-const formatDate = (date) => {
-	return format(new Date(date), 'MMM yyyy')
-}
+import SeeMoreText from './SeeMoreText'
+import { formatDate, getCategoryId } from '../../lib/utils'
+import { StyledCategoryTag, StyledInline } from '../../styles/SharedComponents'
+import BadgeLink from './BadgeLink'
+
+const StyledProject = styled.div`
+	height: fit-content;
+	display: relative;
+`
+
+const StyledDateRange = styled.h5`
+	font-size: 1.2em;
+`
 
 const Project = ({
 	title,
@@ -14,30 +23,63 @@ const Project = ({
 	description,
 	moreLink,
 	mainImg,
-	thumbnailImg,
-}) => (
-	<Col md={12} className='project-container'>
-		<h2>{title}</h2>
-		<Row>
-			<Col md={6} className='my-2'>
-				<Image src={mainImg} alt='Main project image' fluid />
-			</Col>
-			<Col md={6} className='my-2'>
-				<p>{description}</p>
-				<div class='pt-3'>
-					{formatDate(dateStarted)} -{' '}
-					{dateEnded ? formatDate(dateEnded) : 'Current'}
-					{moreLink && (
-						<Badge className='ml-3' variant='primary'>
-							<a target='_blank' href={moreLink}>
-								See more
-							</a>
-						</Badge>
-					)}
-				</div>
-			</Col>
-		</Row>
-	</Col>
-)
+	// thumbnailImg,
+	categories,
+	handleContentLoaded,
+	handleResize
+}) => {
+	const imageRef = useRef(null);
+
+	const dateRange = <div className="d-inline">
+		{formatDate(dateStarted)}{' - '}{dateEnded ? formatDate(dateEnded) : 'Current'}
+	</div>
+
+	const extras = (
+		<div className="pt-3 d-inline">
+			{dateRange}
+		</div>
+	)
+
+	const categoryTags = categories.map(({ name }) => (
+		<StyledCategoryTag
+			as={Badge}
+			key={name}
+			variant="info"
+		>#{name}</StyledCategoryTag>
+	))
+
+	const categoryClasses = categories.map(({ id }) => getCategoryId(id)).join(' ')
+
+	return (
+		<StyledProject
+			as={Col}
+			className={`project-container ${categoryClasses}`}
+		>
+			<h2>{title}</h2>
+			<div className="d-flex flex-wrap align-items-center">
+				<StyledDateRange className="px-1 d-inline">({dateRange})</StyledDateRange>
+				{moreLink && <BadgeLink url={moreLink} className="ml-3">View Website</BadgeLink>}
+			</div>
+			<div>{categoryTags}</div>
+			<Row>
+				<Col lg={6} className="my-2">
+					<Image
+						ref={imageRef}
+						src={mainImg}
+						alt="Main project image"
+						fluid
+						onLoad={handleContentLoaded}
+					/>
+				</Col>
+				<Col lg={6} className="my-2">
+					<SeeMoreText
+						text={description}
+						onToggle={handleResize}
+					/>
+				</Col>
+			</Row>
+		</StyledProject>
+	)
+}
 
 export default Project
