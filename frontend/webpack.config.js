cebdefcde1f651a2db3b4d5ba715d1b4cfa10e10
Eron Salling <esalling23@gmail.com>
2024-05-07 12:45:51 -0400

    Frontend package, webpack updates

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,6 +30,14 @@ module.exports = {
 					loader: 'babel-loader',
 				},
 			},
+			{
+				test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+				loader: require.resolve('url-loader'),
+				options: {
+					limit: 10000,
+					name: 'static/media/[name].[hash:8].[ext]',
+				}
+			}
 		],
 	},
 }
