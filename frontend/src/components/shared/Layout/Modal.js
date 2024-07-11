ab8023ad12a564595ba6d6639eb92f7546e84929
Eron Salling <esalling23@gmail.com>
2024-07-11 18:35:57 -0400

    Adds shared modal component

---
+++ frontend/src/components/shared/Layout/Modal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Modal as BootstrapModal } from 'react-bootstrap';
+
+const Modal = ({
+	title,
+	bodyContent,
+	footerContent,
+	isModalOpen,
+	styles,
+	closeModal
+}) => {
+	return <BootstrapModal show={isModalOpen} className={styles}>
+		<BootstrapModal.Header closeButton>
+			<BootstrapModal.Title>{title}</BootstrapModal.Title>
+		</BootstrapModal.Header>
+
+		<BootstrapModal.Body>
+			{bodyContent}
+		</BootstrapModal.Body>
+
+		<BootstrapModal.Footer>
+			{footerContent || (
+				<Button variant="primary" onClick={closeModal}>Okay</Button>
+			)}
+		</BootstrapModal.Footer>
+	</BootstrapModal>;
+};
+
+Modal.propTypes = {
+	title: PropTypes.element | PropTypes.string,
+	bodyContent: PropTypes.element | PropTypes.string,
+	footerContent: PropTypes.element | PropTypes.string,
+	isModalOpen: PropTypes.bool,
+	styles: PropTypes.any,
+	closeModal: PropTypes.func
+};
+
+export default Modal;
