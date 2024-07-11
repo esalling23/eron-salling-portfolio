e633fa51415c7756b8fe81494b00f99919bb2d04
Eron Salling <esalling23@gmail.com>
2024-07-11 17:47:49 -0400

    Adjusts categories filter loading state

--- frontend/src/components/shared/CategoriesFilter/index.js
+++ frontend/src/components/shared/CategoriesFilter/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton, Spinner } from 'react-bootstrap';
 
 import { getCategoryId } from '../../../lib/utils';
 import styles from './styles.module.scss';
 
 const CategoriesFilter = ({
 	categories,
+	isLoading = false,
 	handleFilter
 }) => {
 	const displayCategoryButton = ({ name, id }) => (
@@ -18,22 +19,34 @@ const CategoriesFilter = ({
 			{name}
 		</Dropdown.Item>
 	);
-	const categoryTags = categories.map(({ name, id }) => 
+	const categoryTags = categories?.map(({ name, id }) => 
 		displayCategoryButton({ name, id: getCategoryId(id) }));
 	
 	return (
 		<DropdownButton 
-			title="Filter Projects"
+			title={isLoading ? <Spinner
+				as="span"
+				animation="border"
+				size="sm"
+				role="status"
+				aria-hidden="true"
+			/> : 'Filter Projects'}
 			className={styles.filterButton}
+			disabled={isLoading}
 		>
-			{displayCategoryButton({ name: 'All Projects', id: '*' })}
-			{categoryTags}
+			{!isLoading && (
+				<>
+					{displayCategoryButton({ name: 'All Projects', id: '*' })}
+					{categoryTags}
+				</>
+			)}
 		</DropdownButton>
 	);
 };
 
 CategoriesFilter.propTypes = {
 	categories: PropTypes.array,
+	isLoading: PropTypes.bool,
 	handleFilter: PropTypes.function
 };
 
