9f14be4966ea34841d309a1ff92e7cc1342ac801
Eron Salling <esalling23@gmail.com>
2024-07-11 17:49:19 -0400

    Adds Arcade nested route handling for games

--- frontend/src/components/pages/Arcade.js
+++ frontend/src/components/pages/Arcade.js
@@ -1,14 +1,43 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import PageContainer from '../shared/PageContainer';
+import { Link, Route, Routes } from 'react-router-dom';
+
 import TileMatch from '../arcade/TileMatch';
+import { Button } from 'react-bootstrap';
+
+const games = {
+	tileMatch: {
+		name: 'Tile Match',
+		path: 'tile-match',
+		component: TileMatch
+	}
+};
 
 const Arcade = () => {
 	return (
-		<PageContainer className='d-flex justify-content-center'>
-			<section className="full-screen d-flex" style={{ width: 'fit-content' }}>
-				<TileMatch />
-			</section>
+		<PageContainer className='flex-col-center'>
+			<Routes>
+				<Route path='' key='arcade-select' element={(
+					<section className="h-100 w-100 d-flex flex-row" style={{ width: 'fit-content' }}>
+						{Object.values(games).map(({ name, path }) => (
+							<Button key={name} as={Link} to={`${path}`}>
+								{name}
+							</Button>
+						))}
+					</section>
+				)} />
+				{Object.values(games).map(({ name, component: Component, path }) => (
+					<Route
+						key={name}
+						element={(
+							<section className="full-screen w-100" key={name}>
+								<Component />
+							</section>
+						)}
+						path={`${path}`} />
+				))}
+			</Routes>
 		</PageContainer>
 	);
 };
