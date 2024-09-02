f6751c0a10e7f23f2c6e7b7744b2eb6cc36169d4
Eron Salling <esalling23@gmail.com>
2024-09-02 13:46:38 -0400

    Updates deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,18 @@
 sudo apt-get update
+echo "Clearing Git status..."
+git stash --include-untracked
+
+echo "Updating files..."
 git pull origin main
+
 pipenv shell "pipenv install && python manage.py migrate && exit"
-# cd frontend && rm -rf node_modules package-lock.json && npm install && npm run build && cd ../
+
+echo "Building app..." 
+cd frontend 
+npm run build
 
 cd ~/ && rm -rf /var/www/eronsalling.me
-cp -rf ~/eron-salling-portfolio /var/www/eronsalling.me
+cp -rf ~./eron-salling-portfolio /var/www/eronsalling.me
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
 echo "Finished"
--- deploy/pre-deploy.sh
+++ deploy/pre-deploy.sh
@@ -2,8 +2,6 @@
 
 echo "Installing packages..."
 cd frontend && rm -rf node_modules package-lock.json && npm install
-echo "Building app..." 
-npm run build
 
 echo "Collecting static files..."
 cd ../
